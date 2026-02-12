27124b6af83a868e2afc8ac2fdb335ff67a518f6
Callme-VR <176268004+Callme-VR@users.noreply.github.com>
2026-02-12 16:49:25 +0530

    add  techincal Specification and api key configruartions

--- Agent_teams_LLm_app/ai_Service_agent/agent.py
+++ Agent_teams_LLm_app/ai_Service_agent/agent.py
@@ -1,5 +1,7 @@
 import os
 from typing import Literal
+
+from plotly.graph_objs import layout
 from agency_swarm import Agent, Agency, BaseTool, ModelSettings
 from pydantic import Field
 import streamlit as st
@@ -62,4 +64,83 @@ class AnalyzeProjectRequirements(BaseTool):
 
 
 class CreateTechnicalSpecification(BaseTool):
+    """Create Technical Specification"""
+    
+    artectecture_type=Literal[
+         "Monolithic",
+         "Microservices",
+         "Serverless",
+         "Event-driven",
+         "Other"
+    ] = Field(..., description="Type of the Architecture")
+    
+    core_technologies: List[str] = Field(..., description="Core Technologies")
+    
+    scalability_features: Literal[
+        "Low",
+        "Medium",
+        "High"
+    ] = Field(..., description="Scalability")
+    
+    class ToolConfig:
+        name = "Create Technical Specification"
+        description = "Create Technical Specification"
+        on_call_at_a_time = True
+        
+    def run(self) -> str:
+        """Creating Technical Specification"""
+        
+        project_analysis=self.context.get("project_analysis", None)
+        
+        
+        if project_analysis is None:
+            raise ValueError("Project analysis is not stored in state")
+       
+        specification={
+            project_name:project_analysis["name"],
+            architecture_type:self.artectecture_type,
+            technologies:self.core_technologies.spli(","),
+            scalability_features:self.scalability_features,
+       }
+        self.context.set("specification", specification)
+        return "Technical specification created successfully{project_analysis['name']}"
+   
+   
+   
+def init_session_state()->None:
+     """Initialize session state"""
+     if "message" not in st.session_state:
+         st.session_state.message=[]
+     if "api_key" not in st.session_state:
+         st.session_state.api_key=None
+        
+        
+def main()->None:
+     st.page_config(page_title="AI Service Agent", layout="wide", page_icon=":robot_face:")
+     init_session_state()
+     
+     # api configuration
+     
+     with st.sidebar:
+          st.header("API Service Agent")
+          st.subheader("API Configuration")
+          api_key=st.text_input("API Key", type="password", value=st.session_state.api_key, on_change=init_session_state)
+          
+          if api_key:
+               st.session_state.api_key=api_key
+               st.success("API Key configured successfully")
+               
+          else:
+               st.warning("API Key is required")
+               st.markdown("[Get your API key here](https://platform.openai.com/api-keys)")
+               return
+          
+     # agent configuration using swarn v1
+     os.environ["OPENAI_API_KEY"] = st.session_state.api_key
+     
+     # with project input Form
+     
+     with st.form(key="project_form"):
+          st.subheader("Project Input")
+     
      
